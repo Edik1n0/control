c5899138cba42360cea20338a09e542b08846051
K1N0 <edisonupb2013@gmail.com>
2020-05-23 17:59:22 -0500

    resta-control

--- control/includes/footer.php
+++
@@ -1,5 +0,0 @@
-    <footer class="f__cont">
-        <a href="https://asesoriasjuridicasmed.com" target="_blank">Asesorías Jurídicas Medellín</a>
-    </footer>
-</body>
-</html>
--- control/index.php
+++
@@ -1,4 +0,0 @@
-<?php include("db.php") ?>
-<?php include("includes/header.php") ?>
-<?php include("includes/access.php") ?>
-<?php include("includes/footer.php") ?>
